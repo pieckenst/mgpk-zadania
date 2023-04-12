b7d933d7065d7b6e47b248bedd9e5cc7bccff8a6
andrey savich <Yvazhaikorova@outlook.com>
2023-04-12 21:55:50 +0300

    prelimenary win 11 theming

---
+++ kursovaya/avtoparkavtobusov/SQLServerLogin/Controls/DarkAwareControls.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Rectify11Installer.Core;
+
+namespace Rectify11Installer.Controls
+{
+	public class DarkAwareTreeView : TreeView
+	{
+		private const int WM_LBUTTONDBLCLK = 0x0203;
+		[DllImport("uxtheme.dll", CharSet = CharSet.Unicode)]
+		private static extern int SetWindowTheme(IntPtr hWnd, string pszSubAppName, string pszSubIdList);
+		// hack to disable artifacts
+		protected override CreateParams CreateParams
+		{
+			get
+			{
+				var cp = base.CreateParams;
+				cp.ExStyle |= 0x02000000; //WS_EX_COMPOSITED
+				return cp;
+			}
+		}
+		public DarkAwareTreeView()
+		{
+			Theme.OnThemeChanged += delegate
+			{
+				UpdateTheming();
+			};
+		}
+		protected override void WndProc(ref Message m)
+		{
+			if (m.Msg == WM_LBUTTONDBLCLK)
+			{
+				var info = HitTest(PointToClient(Cursor.Position));
+				if (info.Location == TreeViewHitTestLocations.StateImage)
+				{
+					m.Result = IntPtr.Zero;
+					return;
+				}
+			}
+			base.WndProc(ref m);
+		}
+		protected override void CreateHandle()
+		{
+			base.CreateHandle();
+			UpdateTheming();
+		}
+		private void UpdateTheming()
+		{
+			if (Theme.IsUsingDarkMode)
+			{
+				SetWindowTheme(Handle, "DarkMode_Explorer", null);
+				ForeColor = Color.White;
+				BackColor = Color.Black;
+			}
+			else
+			{
+				SetWindowTheme(Handle, "explorer", null);
+				ForeColor = Color.Black;
+				BackColor = Color.White;
+			}
+		}
+	}
+	public class DarkAwareLabel : Label
+	{
+		public DarkAwareLabel()
+		{
+			Theme.OnThemeChanged += delegate
+			{
+				UpdateTheming();
+			};
+		}
+		protected override void CreateHandle()
+		{
+			base.CreateHandle();
+			UpdateTheming();
+		}
+		private void UpdateTheming()
+		{
+			if (Theme.IsUsingDarkMode)
+			{
+				BackColor = Color.Black;
+				ForeColor = Color.White;
+			}
+			else
+			{
+				BackColor = Color.White;
+				ForeColor = Color.Black;
+			}
+		}
+	}
+	public class DarkAwareRichTextBox : RichTextBox
+	{
+		public DarkAwareRichTextBox()
+		{
+			Theme.OnThemeChanged += delegate
+			{
+				UpdateTheming();
+			};
+		}
+		protected override void CreateHandle()
+		{
+			base.CreateHandle();
+			UpdateTheming();
+		}
+		private void UpdateTheming()
+		{
+			if (Theme.IsUsingDarkMode)
+			{
+				BackColor = Color.Black;
+				ForeColor = Color.White;
+			}
+			else
+			{
+				BackColor = Color.White;
+				ForeColor = Color.Black;
+			}
+		}
+	}
+
+	public class DarkAwareTabPage : TabPage
+	{
+		public DarkAwareTabPage()
+		{
+			Theme.OnThemeChanged += delegate
+			{
+				UpdateTheming();
+			};
+		}
+		protected override void CreateHandle()
+		{
+			base.CreateHandle();
+			UpdateTheming();
+		}
+		private void UpdateTheming()
+		{
+			if (Theme.IsUsingDarkMode)
+			{
+				BackColor = Color.Black;
+				ForeColor = Color.White;
+			}
+			else
+			{
+				BackColor = Color.White;
+				ForeColor = Color.Black;
+			}
+		}
+	}
+	public class DarkAwareRadioButton : RadioButton
+	{
+		[DllImport("uxtheme.dll", CharSet = CharSet.Unicode)]
+		private static extern int SetWindowTheme(IntPtr hWnd, string pszSubAppName, string pszSubIdList);
+		public DarkAwareRadioButton()
+		{
+			Theme.OnThemeChanged += delegate
+			{
+				UpdateTheming();
+			};
+		}
+		protected override void CreateHandle()
+		{
+			base.CreateHandle();
+			UpdateTheming();
+		}
+		private void UpdateTheming()
+		{
+			if (Theme.IsUsingDarkMode)
+			{
+				SetWindowTheme(Handle, "DarkMode_Explorer", null);
+				ForeColor = Color.White;
+			}
+			else
+			{
+				SetWindowTheme(Handle, "Explorer", null);
+				ForeColor = Color.Black;
+			}
+		}
+	}
+	public class DarkAwareCheckBox : CheckBox
+	{
+		[DllImport("uxtheme.dll", CharSet = CharSet.Unicode)]
+		private static extern int SetWindowTheme(IntPtr hWnd, string pszSubAppName, string pszSubIdList);
+		public DarkAwareCheckBox()
+		{
+			Theme.OnThemeChanged += delegate
+			{
+				UpdateTheming();
+			};
+		}
+		protected override void CreateHandle()
+		{
+			base.CreateHandle();
+			UpdateTheming();
+		}
+		private void UpdateTheming()
+		{
+			if (Theme.IsUsingDarkMode)
+			{
+				SetWindowTheme(Handle, "DarkMode_Explorer", null);
+				ForeColor = Color.White;
+			}
+			else
+			{
+				SetWindowTheme(Handle, "Explorer", null);
+				ForeColor = Color.Black;
+			}
+		}
+	}
+	public class DarkAwareComboBox : ComboBox
+	{
+		[DllImport("uxtheme.dll", CharSet = CharSet.Unicode)]
+		private static extern int SetWindowTheme(IntPtr hWnd, string pszSubAppName, string pszSubIdList);
+		public DarkAwareComboBox()
+		{
+			Theme.OnThemeChanged += delegate
+			{
+				UpdateTheming();
+			};
+		}
+		protected override void CreateHandle()
+		{
+			base.CreateHandle();
+			UpdateTheming();
+		}
+		private void UpdateTheming()
+		{
+			if (Theme.IsUsingDarkMode)
+			{
+				SetWindowTheme(Handle, "DarkMode_CFD", null);
+				ForeColor = Color.White;
+				BackColor = Color.Black;
+			}
+			else
+			{
+				SetWindowTheme(Handle, "CFD", null);
+				ForeColor = Color.Black;
+				BackColor = Color.White;
+			}
+		}
+	}
+
+}
--- kursovaya/avtoparkavtobusov/SQLServerLogin/Program.cs
+++ kursovaya/avtoparkavtobusov/SQLServerLogin/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Runtime;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +19,37 @@ namespace SQLServerLoginTemplate
 		[STAThread]
 		static void Main()
 		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new FormConnectToSQLServer());
+            
+            ProfileOptimization.SetProfileRoot(Path.Combine(Path.GetTempPath(), "Rectify11"));
+            ProfileOptimization.StartProfile("Startup.Profile");
+            if (Environment.OSVersion.Version.Build >= 10240)
+            {
+                Theme.InitTheme();
+                if ((Environment.OSVersion.Version.Build >= 17763) && (Environment.OSVersion.Version.Build < 18362))
+                {
+                    DarkMode.AllowDarkModeForApp(true);
+                }
+                else if (Environment.OSVersion.Version.Build >= 18362)
+                {
+                    DarkMode.SetPreferredAppMode(DarkMode.PreferredAppMode.AllowDark);
+                }
+            }
+            if (!Directory.Exists(Variables.r11Folder))
+            {
+                Directory.CreateDirectory(Variables.r11Folder);
+            }
+
+            if ((!File.Exists(Path.Combine(Variables.r11Folder, "Dark.msstyles"))) && (!File.Exists(Path.Combine(Variables.r11Folder, "light.msstyles"))))
+            {
+                File.WriteAllBytes(Path.Combine(Variables.r11Folder, "Dark.msstyles"), Properties.Resources.Dark);
+                File.WriteAllBytes(Path.Combine(Variables.r11Folder, "light.msstyles"), Properties.Resources.light);
+            }
+            Theme.LoadTheme();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("ko");
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.CurrentUICulture;
+            Application.Run(new FormConnectToSQLServer());
 		}
 	}
 }
