5bf201ee3979e15ab2bd691d13020bdec4db7d24
andrey savich <Yvazhaikorova@outlook.com>
2023-04-13 22:05:22 +0300

    edit

--- kursovaya/avtoparkavtobusov/FluentKursovayaAvtoparkA/obj/Debug/net6.0-windows/Views/Pages/TicketPage.g.i.cs
+++ kursovaya/avtoparkavtobusov/FluentKursovayaAvtoparkA/obj/Debug/net6.0-windows/Views/Pages/TicketPage.g.i.cs
@@ -165,7 +165,7 @@ namespace FluentKursovayaAvtoparkA.Views.Pages {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/FluentKursovayaAvtoparkA;V1.0.0.0;component/views/pages/ticketpage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/FluentKursovayaAvtoparkA;component/views/pages/ticketpage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\Pages\TicketPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
