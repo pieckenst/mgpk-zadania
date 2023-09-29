a6c9605d1a489607b550d34d80ceff261d6e9dce
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2023-09-29 14:42:13 +0300

    Finalish build of 3rd control work

--- c#-kr3/KR2DataBasePoet/Views/Pages/DashboardPage.xaml.cs
+++ c#-kr3/KR2DataBasePoet/Views/Pages/DashboardPage.xaml.cs
@@ -4,7 +4,12 @@
 // All Rights Reserved.
 
 using ComposerAndTheirWorks.ViewModels.Pages;
+using ComposerAndTheirWorks.Views.Windows;
+using System.Data.SqlClient;
+using System.Data;
 using Wpf.Ui.Controls;
+using System;
+using System.Windows.Controls;
 
 namespace ComposerAndTheirWorks.Views.Pages
 {
@@ -18,6 +23,126 @@ namespace ComposerAndTheirWorks.Views.Pages
             DataContext = this;
 
             InitializeComponent();
+        }
+
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SqlConnection cnn;
+                var conStr = MainWindow.formations;
+
+                cnn = new SqlConnection(conStr);
+                cnn.Open();
+                Console.Write("OPENING DB CONNECTION!!! \n");
+                Console.Write("[MENU] Connect db clicked - test \n");
+                var select = "SELECT * FROM ComposerWorksList";
+
+                var commandBuilder = new SqlCommand(select, cnn);
+                commandBuilder.ExecuteNonQuery();
+
+                var dataAdapter = new SqlDataAdapter(commandBuilder);
+                var ds = new DataTable("Avtobusx");
+                dataAdapter.Fill(ds);
+                datagrid1.ItemsSource = ds.DefaultView;
+                Console.WriteLine("Connection established and the datagrid filled!");
+                cnn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Error");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var CompKey = Edit1.Text;
+                var NameOfWork = Edit2.Text;
+                var CreationDate = Edit3.Text;
+                var LinkCompKey = Edit4.Text;
+                var CompNameSurname = Edit5.Text;
+                
+                var query =
+                    "INSERT INTO ComposerWorksList(ComposerWorkKey, WorkName, DateOfCreation, LinkComposerKey,NameAndSurnameComposer) " +
+                    "Values('" + CompKey + "', '" + NameOfWork + "', '" + CreationDate + "', '" + LinkCompKey + "','" +
+                    CompNameSurname + "')";
+                SqlConnection cnn;
+                var conStr = MainWindow.formations;
+
+                cnn = new SqlConnection(conStr);
+                cnn.Open();
+                var command = new SqlCommand(query, cnn);
+                command.ExecuteNonQuery();
+                Console.WriteLine("Data inserted!\n Closing connection ");
+                cnn.Close();
+                Console.WriteLine("Connection has been closed , database ready for next operation!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+    }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            SqlConnection cnn;
+            var conStr = MainWindow.formations;
+
+            cnn = new SqlConnection(conStr);
+            cnn.Open();
+            Console.Write("OPENING DB CONNECTION!!! \n");
+            var select = "DELETE FROM ComposerWorksList WHERE ComposerWorkKey=@a2";
+            var commandBuilder = new SqlCommand(select, cnn);
+            commandBuilder.Parameters.AddWithValue("a2", Edit1.Text);
+            try
+            {
+                commandBuilder.ExecuteNonQuery();
+                Console.WriteLine("Delete operation successful!");
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            
+                try
+                {
+                    SqlConnection cnn;
+                    var conStr = MainWindow.formations;
+
+                    cnn = new SqlConnection(conStr);
+                    cnn.Open();
+                    Console.Write("OPENING DB CONNECTION!!! \n");
+                    Console.Write("[MENU] Connect db clicked - test \n");
+                    var select = "Select * FROM ComposerWorksList WHERE NameAndSurnameComposer Like '%" + searchbox.Text + "%'";
+
+                    var commandBuilder = new SqlCommand(select, cnn);
+                    
+                    commandBuilder.ExecuteNonQuery();
+
+                    var dataAdapter = new SqlDataAdapter(commandBuilder);
+                    var ds = new DataTable("Avtobusx");
+                    dataAdapter.Fill(ds);
+                    datagrid1.ItemsSource = ds.DefaultView;
+                    Console.WriteLine("Connection established and the datagrid filled!");
+                    cnn.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.Write("Error");
+                }
+
+            
+
         }
     }
 }
--- c#-kr3/KR2DataBasePoet/Views/Pages/DataPage.xaml.cs
+++ c#-kr3/KR2DataBasePoet/Views/Pages/DataPage.xaml.cs
@@ -9,6 +9,7 @@ using System.Data.SqlClient;
 using ComposerAndTheirWorks.Views.Windows;
 using System.Data;
 using System.Windows.Controls;
+using System.Windows;
 
 namespace ComposerAndTheirWorks.Views.Pages
 {
@@ -41,6 +42,187 @@ namespace ComposerAndTheirWorks.Views.Pages
                 var select = "SELECT * FROM ComposerList";
 
                 var commandBuilder = new SqlCommand(select, cnn);
+                commandBuilder.ExecuteNonQuery();
+
+                var dataAdapter = new SqlDataAdapter(commandBuilder);
+                var ds = new DataTable("Avtobusx");
+                dataAdapter.Fill(ds);
+                datagrid1.ItemsSource = ds.DefaultView;
+                Console.WriteLine("Connection established and the datagrid filled!");
+                cnn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Error");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var CompKey = Edit1.Text;
+                var NameOfWork = Edit2.Text;
+                var CreationDate = Edit3.Text;
+                var LinkCompKey = Edit4.Text;
+                var CompNameSurname = Edit5.Text;
+                var WorksCount = Edit6.Text;
+                var BirthDate = Edit7.Text;
+                var DeathDate = Edit8.Text;
+                var BirthYear = Edit9.Text;
+                var DeathYear = Edit10.Text;
+                var query =
+                    "INSERT INTO ComposerList(ComposerKey, Name, Surname, MiddleName,Genre,Numberofworks,BirthDate,DeathDate,BirthYear,DeathYear) " +
+                    "Values('" + CompKey + "', '" + NameOfWork + "', '" + CreationDate + "', '" + LinkCompKey + "','" +
+                    CompNameSurname + "','" + WorksCount + "','" + BirthDate + "','" + DeathDate + "', '" + BirthYear + "','" + DeathYear + "' )";
+                SqlConnection cnn;
+                var conStr = MainWindow.formations;
+
+                cnn = new SqlConnection(conStr);
+                cnn.Open();
+                var command = new SqlCommand(query, cnn);
+                command.ExecuteNonQuery();
+                Console.WriteLine("Data inserted!\n Closing connection ");
+                cnn.Close();
+                Console.WriteLine("Connection has been closed , database ready for next operation!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            SqlConnection cnn;
+            var conStr = MainWindow.formations;
+
+            cnn = new SqlConnection(conStr);
+            cnn.Open();
+            Console.Write("OPENING DB CONNECTION!!! \n");
+            var select = "DELETE FROM ComposerList WHERE ComposerKey=@a2";
+            var commandBuilder = new SqlCommand(select, cnn);
+            commandBuilder.Parameters.AddWithValue("a2", Edit1.Text);
+            try
+            {
+                commandBuilder.ExecuteNonQuery();
+                Console.WriteLine("Delete operation successful!");
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SqlConnection cnn;
+                var conStr = MainWindow.formations;
+
+                cnn = new SqlConnection(conStr);
+                cnn.Open();
+                Console.Write("OPENING DB CONNECTION!!! \n");
+                Console.Write("[MENU] Connect db clicked - test \n");
+                var select = "Select * FROM ComposerList WHERE Surname Like '%" + searchbox.Text + "%'";
+
+                var commandBuilder = new SqlCommand(select, cnn);
+
+                commandBuilder.ExecuteNonQuery();
+
+                var dataAdapter = new SqlDataAdapter(commandBuilder);
+                var ds = new DataTable("Avtobusx");
+                dataAdapter.Fill(ds);
+                datagrid1.ItemsSource = ds.DefaultView;
+                Console.WriteLine("Connection established and the datagrid filled!");
+                cnn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Error");
+            }
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SqlConnection cnn;
+                var conStr = MainWindow.formations;
+                var filterquery= "Cочетание Электронной и Классической музыки";
+                cnn = new SqlConnection(conStr);
+                cnn.Open();
+                Console.Write("OPENING DB CONNECTION!!! \n");
+                Console.Write("[MENU] Connect db clicked - test \n");
+                var select = "Select * FROM ComposerList WHERE Genre Like '%" + filterquery + "%'";
+
+                var commandBuilder = new SqlCommand(select, cnn);
+
+                commandBuilder.ExecuteNonQuery();
+
+                var dataAdapter = new SqlDataAdapter(commandBuilder);
+                var ds = new DataTable("Avtobusx");
+                dataAdapter.Fill(ds);
+                datagrid1.ItemsSource = ds.DefaultView;
+                Console.WriteLine("Connection established and the datagrid filled!");
+                cnn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Error");
+            }
+        }
+
+        private void genre3_Checked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SqlConnection cnn;
+                var conStr = MainWindow.formations;
+                var filterquery = "Современная классическая музыка";
+                cnn = new SqlConnection(conStr);
+                cnn.Open();
+                Console.Write("OPENING DB CONNECTION!!! \n");
+                Console.Write("[MENU] Connect db clicked - test \n");
+                var select = "Select * FROM ComposerList WHERE Genre Like '%" + filterquery + "%'";
+
+                var commandBuilder = new SqlCommand(select, cnn);
+
+                commandBuilder.ExecuteNonQuery();
+
+                var dataAdapter = new SqlDataAdapter(commandBuilder);
+                var ds = new DataTable("Avtobusx");
+                dataAdapter.Fill(ds);
+                datagrid1.ItemsSource = ds.DefaultView;
+                Console.WriteLine("Connection established and the datagrid filled!");
+                cnn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Error");
+            }
+        }
+
+        private void genre2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SqlConnection cnn;
+                var conStr = MainWindow.formations;
+                var filterquery = "Классическая музыка";
+                cnn = new SqlConnection(conStr);
+                cnn.Open();
+                Console.Write("OPENING DB CONNECTION!!! \n");
+                Console.Write("[MENU] Connect db clicked - test \n");
+                var select = "Select * FROM ComposerList WHERE Genre Like '%" + filterquery + "%'";
+
+                var commandBuilder = new SqlCommand(select, cnn);
+
                 commandBuilder.ExecuteNonQuery();
 
                 var dataAdapter = new SqlDataAdapter(commandBuilder);
